e3709e4f811f6970a32e1bc7e676ada411516439
etctera <yilonglei790@gmail.com>
2023-03-25 18:15:50 +0800

    chore: function

--- function/src/main.rs
+++ function/src/main.rs
@@ -2,15 +2,35 @@ fn apply(value: i32, f: fn(i32) -> i32) -> i32 {
   f(value)
 }
 
-fn square(value:i32) -> i32 {
+fn square(value: i32) -> i32 {
   value * value
 }
 
-fn cube(value:i32) -> i32 {
+fn cube(value: i32) -> i32 {
   value * value * value
 }
 
+fn pi() -> f64 {
+  3.1415926
+}
+
+fn not_pi() {
+  3.1415926;
+}
+
 fn main() {
+  let is_pi = pi();
+  let is_unit1 = not_pi();
+  let is_unit2 = {
+    pi();
+  };
+
+  println!(
+    "is_pi: {:?}, is_unit1: {:?}, is_unit2: {:?}",
+    is_pi,
+    is_unit1,
+    is_unit2,
+  );
   println!("apply square: {}", apply(2, square));
   println!("apply cube: {}", apply(2, cube));
 }
